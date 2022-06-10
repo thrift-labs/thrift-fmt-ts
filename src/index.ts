5df97b86d78d722f6bdfb9fde24a5d9d9ae04c4d
alingse <alingse@foxmail.com>
2022-06-10 09:27:55 +0800

    add ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export function hello(): string {
-    return "hello world";
+import { ThriftData } from 'thrift-parser-antlr4';
+
+export function hello(): ThriftData {
+    return ThriftData.from_str('hello world');
+}
+
+export class PureThriftFormatter {
+
 }
