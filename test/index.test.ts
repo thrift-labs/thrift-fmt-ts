e41c2555dbb4864b4485b51a0da9ce94706789e1
alingse <alingse@foxmail.com>
2022-11-27 20:30:16 +0800

    fix bug && add splitFieldByAssign

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,6 +3,7 @@ import assert  from 'assert';
 
 import { ThriftData } from 'thrift-parser-ts';
 import { PureThriftFormatter, ThriftFormatter, newOption } from '../src/index'
+import { splitFieldByAssign } from '../src/index'
 import * as ThriftParserNS from "thrift-parser-ts/lib/ThriftParser";
 
 describe('Test PureThriftFormatter', () => {
@@ -97,7 +98,7 @@ struct Work {
         const rawThrift = `
         struct Work {
             1: i32 num1 = 0;
-            2: i32 num2,
+            2: i32 number2,
             3: i32 num3
         }`;
         const data = ThriftData.from_string(rawThrift);
@@ -106,7 +107,7 @@ struct Work {
         const thrift = fmt.format();
 const expectThrift = `struct Work {
     1: required i32 num1 = 0,
-    2: required i32 num2,
+    2: required i32 number2,
     3: required i32 num3,
 }`
         assert.equal(thrift, expectThrift);
@@ -173,7 +174,28 @@ describe('test ThriftFormatter with assign algin', () => {
         assert.ok(fileds[0] instanceof ThriftParserNS.FieldContext);
         assert.ok(fileds[1] instanceof ThriftParserNS.FieldContext);
         const [p1, p2] = fmt.calc_subblocks_padding(fileds);
-        assert.equal(p1, 23)
+        assert.equal(p1, 21)
+        assert.equal(p2, 25)
+    })
+    it('with calc padding v2', () => {
+        const rawThrift = `
+        struct Work {
+        1: i32 num1 = 0,
+        2: required i32 numbers, // num2 for
+        }`;
+        const data = ThriftData.from_string(rawThrift);
+        const fmt = new ThriftFormatter(data);
+        fmt.option(newOption({assignAlign: true}))
+
+        assert.equal(data.document.childCount, 2, data.toString());
+        const structValue = <ThriftParserNS.Struct_Context>data.document.getChild(0).getChild(0);
+        assert.equal(structValue.childCount, 6, structValue.toString());
+        const fileds = structValue.children!.slice(3, 5)!;
+        assert.equal(fileds.length, 2);
+        assert.ok(fileds[0] instanceof ThriftParserNS.FieldContext);
+        assert.ok(fileds[1] instanceof ThriftParserNS.FieldContext);
+        const [p1, p2] = fmt.calc_subblocks_padding(fileds);
+        assert.equal(p1, 24)
         assert.equal(p2, 28)
     })
 
@@ -182,11 +204,13 @@ describe('test ThriftFormatter with assign algin', () => {
         enum NUM {
             ONE =1,
             SEVEN = 7,
+            ELEVLEN,
         }
 
         struct Work {
         1: i32 num1 = 0,
         2: required string username = "hello", // name
+        3: optional list<map<string, bool>> attrs;
         }`;
         const data = ThriftData.from_string(rawThrift);
         const fmt = new ThriftFormatter(data);
@@ -194,14 +218,64 @@ describe('test ThriftFormatter with assign algin', () => {
         const out = fmt.format();
 
         assert.equal(out, `enum NUM {
-    ONE   = 1,
-    SEVEN = 7,
+    ONE     = 1,
+    SEVEN   = 7,
+    ELEVLEN,
 }
 
 struct Work {
-    1: required i32 num1        = 0,
-    2: required string username = "hello",  // name
+    1: required i32 num1                      = 0,
+    2: required string username               = "hello", // name
+    3: optional list<map<string, bool>> attrs,
+}`);
+
+    })
+
+    it('check assginsimple', () => {
+        const rawThrift = `
+        enum NUM {
+            ONE = 1,
+            SEVEN = 7,
+            ELEVLEN,
+        }`;
+        const data = ThriftData.from_string(rawThrift);
+        const fmt = new ThriftFormatter(data);
+        fmt.option(newOption({assignAlign: true}))
+        const out = fmt.format();
+
+        assert.equal(out, `enum NUM {
+    ONE     = 1,
+    SEVEN   = 7,
+    ELEVLEN,
 }`);
 
     })
 });
+
+describe('test some function', () =>{
+    it('check splitFieldByAssign', () => {
+        const rawThrift = `
+        struct Work {
+        1: i32 num1 = 0,
+        2: required i32 numbers, // num2 for
+        }`;
+        const data = ThriftData.from_string(rawThrift);
+        const fmt = new ThriftFormatter(data);
+        fmt.option(newOption({assignAlign: true}))
+
+        assert.equal(data.document.childCount, 2, data.toString());
+        const structValue = <ThriftParserNS.Struct_Context>data.document.getChild(0).getChild(0);
+        const fieldValue = <ThriftParserNS.FieldContext>structValue.getChild(3);
+        const [left, right] = splitFieldByAssign(fieldValue);
+
+        assert.ok(left instanceof ThriftParserNS.FieldContext)
+        assert.ok(right instanceof ThriftParserNS.FieldContext)
+        assert.equal(left.childCount, 3);
+        assert.equal(right.childCount, 3);
+
+        const leftValue = new PureThriftFormatter().format_node(left);
+        const rightValue = new PureThriftFormatter().format_node(right);
+        assert.equal(leftValue, `1: i32 num1`)
+        assert.equal(rightValue, `= 0,`)
+    })
+})
